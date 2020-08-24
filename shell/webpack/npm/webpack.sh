e96d1ec08ce5dc16a3906fcfaccb3723266f51df
Mahmoud Zakria <me@zakcoding.com>
2020-08-25 01:12:11 +0200

    added shell cmd to webpack

--- shell/webpack/npm/webpack.sh
+++ shell/webpack/npm/webpack.sh
@@ -1,1 +1,2 @@
+#!/bin/zsh
 npm i -D webpack webpack-cli sass sass-loader css-loader postcss-loader autoprefixer file-loader html-webpack-plugin  webpack-dev-server clean-webpack-plugin cross-env
--- shell/webpack/yarn/webpack.sh
+++ shell/webpack/yarn/webpack.sh
@@ -1,1 +1,2 @@
+#!/bin/zsh
 yarn add -D webpack webpack-cli sass sass-loader css-loader postcss-loader autoprefixer file-loader html-webpack-plugin  webpack-dev-server clean-webpack-plugin cross-env
